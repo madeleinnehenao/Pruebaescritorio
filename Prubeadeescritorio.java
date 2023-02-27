ea009ce7ec8620c88cd54f29ff7971f06bf7ff2a
madeleinnehenao <mhenaoga@unal.edu.co>
2023-02-27 15:54:00 -0500

    agregar codigo

---
+++ Prubeadeescritorio.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios1semana;
+
+/**
+ *
+ * @author PAULINA HENAO GARCIA
+ */
+public class Prubeadeescritorio {
+    public static void main(String[] args){
+      float SUMA=0;
+      float x=20;
+      SUMA=SUMA + (x);
+      float Y=40;
+      x= x+ (int) Math.pow(Y,2);
+      SUMA=SUMA + (x/Y);
+      System.out.println("El valor de la suma es:" + SUMA);
+  }
+}   
